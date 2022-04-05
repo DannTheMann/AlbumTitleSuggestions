97c2aadbaa3ab113d713292d284201fb40be1f2d
DannTheMann <dja33@kent.ac.uk>
2022-04-05 22:09:53 +0100

    Updates

---
+++ precache-manifest.85a808044dc770bfe92b57336ff5d787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3563436b0d18fea4b001",
+    "url": "/my-app/static/css/main.10501430.chunk.css"
+  },
+  {
+    "revision": "3563436b0d18fea4b001",
+    "url": "/my-app/static/js/main.bf33ea16.chunk.js"
+  },
+  {
+    "revision": "3126db65ec15d25a5521",
+    "url": "/my-app/static/js/runtime~main.eebd60f5.js"
+  },
+  {
+    "revision": "70c8b41ede6f70e6e1ec",
+    "url": "/my-app/static/js/2.a962c0fb.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/my-app/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "543e84b936ca12496f2aa1133ef81cd9",
+    "url": "/my-app/index.html"
+  }
+];
